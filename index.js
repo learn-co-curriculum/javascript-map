12d672ec7e0680ef9c62cb773050975958b7b0a7
Thomas Tuts <thomas@thomastuts.com>
2016-07-12 18:02:57 +0200

    Add solution code (#2)
    * Add first draft of content and accompanying code
    
    * Remove solution code for now
    
    * Add solution code

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-const app = "I don't do much."
+function map(collection, callback) {
+  const result = [];
+
+  for (let i = 0; i < collection.length; i++) {
+    const element = collection[i];
+    result.push(callback(element));
+  }
+
+  return result;
+}
+
+const numbers = [1, 2, 3];
+const doubledNumbers = map(numbers, function (number) {
+  return number * 2;
+});
+console.log(doubledNumbers); // prints [2, 4, 6]
+
+const autobots = [
+  { name: 'Optimus Prime', strength: 5, isTransformed: false, },
+  { name: 'Ironhide', strength: 3, isTransformed: false, },
+  { name: 'Bumblebee', strength: 2.5, isTransformed: false, },
+  { name: 'Ratchet', strength: 1.5, isTransformed: false, },
+];
+
+const transformedAutobots = map(autobots, function (autobot) {
+  return Object.assign({}, autobot, {
+    strength: autobot.strength * 2,
+    isTransformed: true,
+  });
+});
+
+console.log(transformedAutobots);
+/**
+ Prints:
+
+ [ { name: 'Optimus Prime', strength: 10, isTransformed: true },
+ { name: 'Ironhide', strength: 6, isTransformed: true },
+ { name: 'Bumblebee', strength: 5, isTransformed: true },
+ { name: 'Ratchet', strength: 3, isTransformed: true } ]
+ */
+
+const transformedAutobotsWithMap = autobots.map(function (autobot) {
+  return Object.assign({}, autobot, {
+    strength: autobot.strength * 2,
+    isTransformed: true,
+  });
+});
+
+console.log(transformedAutobotsWithMap);
+/**
+ Prints:
+
+ [ { name: 'Optimus Prime', strength: 10, isTransformed: true },
+ { name: 'Ironhide', strength: 6, isTransformed: true },
+ { name: 'Bumblebee', strength: 5, isTransformed: true },
+ { name: 'Ratchet', strength: 3, isTransformed: true } ]
+ */
